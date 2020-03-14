220114ab1823a234e5fc3bc393c21a0a89712655
sbmvirdi <shubhamvirdic3@gmail.com>
2020-03-14 15:22:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hms/HeadStaff/HeadStaffActivity.java
@@ -0,0 +1,16 @@
+package com.example.hms.HeadStaff;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hms.R;
+
+public class HeadStaffActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_head_staff);
+    }
+}
