45c658482efa0087eebec362522d597c61f116d8
sbmvirdi <shubhamvirdic3@gmail.com>
2020-03-14 16:34:11 +0530

    splash screen

--- app/src/main/java/com/example/hms/MainActivity.java
+++ app/src/main/java/com/example/hms/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.hms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.hms.User.UserActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, UserActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },1000);
     }
 }
